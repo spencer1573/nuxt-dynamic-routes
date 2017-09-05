9b060cdba4a2347b909a9fb5a4b2d9815444ef41
Auzy Stephens <auzy@auzy.me>
2017-09-05 13:34:37 -0600

    nuxt title definition

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,17 @@ module.exports = {
   /*
   ** Headers of the page
   */
+  head: {
+    title: '%',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'nauvoo pageant invite' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
   /*
   ** Customize the progress-bar color
   */
