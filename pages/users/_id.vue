8765e45ebf9883b8f79cab25cf6a7d2d341ae6ad
spencer1573 <spencer1573@users.noreply.github.com>
2017-09-05 12:42:45 -0600

    got title to change according to data

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,29 +1,53 @@
 <template>
   <div class="user">
-    <h3>{{ name }}</h3>
+    <!-- <h3>{{ name }}</h3>
     <h4>@{{ username }}</h4>
     <p>Email : {{ email }}</p>
-    <p><nuxt-link to="/">List of users</nuxt-link></p>
+    <p><nuxt-link to="/">List of users</nuxt-link></p> -->
+    <a @click="getPerson()">click</a>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
+  data () {
+    return {
+      title: 's'
+    }
+  },
   validate ({ params }) {
     return !isNaN(+params.id)
   },
+  computed: {
+    ...mapGetters([
+      'people'
+    ])
+  },
   props: ['person'],
-  head: {
-    title: this.id,
-    meta: [
-      {
-        id: 'description',
-        name: 'description',
-        content: 'My about page'
-      }
-    ]
+  head () {
+    return {
+      title: this.title,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description'
+        }
+      ]
+    }
+  },
+  methods: {
+    getPerson () {
+      console.log('people', this.people)
+      console.log('person', this.people[0].name)
+    },
+    ...mapActions([
+      'loadPeopleList'
+    ])
+
   },
   async asyncData ({ params, error }) {
     try {
@@ -32,7 +56,13 @@ export default {
     } catch (e) {
       error({ message: 'User not found', statusCode: 404 })
     }
+  },
+  mounted () {
+    this.loadPeopleList()
+    this.title = this.people[0].name
+    // this.title = 's2'
   }
+
 }
 </script>
 
